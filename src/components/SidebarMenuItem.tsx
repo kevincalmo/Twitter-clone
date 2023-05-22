57e3bb2b5ebc177ca91367dfe77d37268a631927
Kévin F. CALMO <kevinfcalmo@gmail.com>
2023-05-22 17:12:27 -0400

    Create Sidebar component

---
+++ src/components/SidebarMenuItem.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function SidebarMenuItem({
+  text,
+  Icon,
+  active,
+}: {
+  text: string;
+  Icon: any;
+  active: boolean;
+}) {
+  return (
+    <div className="hoverEffect flex items-center text-gray-700 justify-center xl:justify-start text-lg space-x-3">
+      <Icon className="h-7" />
+      <span className={`${active && "font-bold"} hidden xl:inline`}>
+        {text}
+      </span>
+    </div>
+  );
+}
